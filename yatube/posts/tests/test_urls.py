e4d1566d9c01371a9a9fc6b9fde081f97ca33fd6
Ершов Тимофей <eqeq.tima@yandex.ru>
2022-06-09 13:34:49 +0300

    404

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -98,3 +98,16 @@ class PostsURLSandTemplatesTest(TestCase):
             with self.subTest(address=address):
                 response = self.authorized_client_author.get(address)
                 self.assertTemplateUsed(response, template)
+
+
+class PostsPagesTests(TestCase):
+    def setUp(self):
+        super().setUpClass()
+        self.user_1 = User.objects.create_user(username='user_1')
+        self.authorized_user_1 = Client()
+        self.authorized_user_1.force_login(self.user_1)
+
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон"""
+        response = self.authorized_user_1.get('/404pagenotfound/')
+        self.assertTemplateUsed(response, 'core/404.html')
